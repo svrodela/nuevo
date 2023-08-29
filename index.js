b3d892af9ec58b1b75339a7c79d389b33b860865
svrodela <svrodela@gmail.com>
2023-08-29 14:48:38 -0600

    creacion de bd

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hola!")
+
+var a=2, b=5, r,s="Resto= "
+r= a <<= 8
+
+const persona ={
+    nombre:"Sergio",
+    apellidoPaterno:"Valdez",
+    apellidoMaterno:"Rodela",
+    semestre:7
+}
+
+const fruta=["naranja","manzana","platano","tomate",2,8,new Date(),{
+    nombre:"Sergio",
+    apellidoPaterno:"Valdez",
+    apellidoMaterno:"Rodela",
+    semestre:7
+}]
+//console.log(fruta[6])
+
+function promedio(u1,u2,u3,u4){
+    let prom = (u1+u2+u3+u4)/4
+    return prom
+}
+
+console.log(promedio(8,9,7,9.5))
+
+
+
+
+
