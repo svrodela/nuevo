56dd99c3a464aa2c54726093b8c61e734762eb51
svrodela <svrodela@gmail.com>
2023-09-12 15:52:55 -0600

    agregamos conexion de bd, creamos modelo y vista

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const express = require('express'); // referencia a framework express
 const app = express(); //se crea la aplicación de express
 const log = require('morgan'); // para saber los clientes conectados
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const IndexRoutes = require('./routers/index.js');
+const { default: mongoose } = require('mongoose');
 
-app.set('port', process.env.PORT ||  3000 ); //asigno puerto 3000
+
+app.set('port', process.env.PORT || 3000 ); //asigno puerto 3000
 
 //Middleware utiliza morgan
 app.use(log('dev'));
@@ -18,4 +21,16 @@ app.listen(app.get('port'), () =>{
     console.log('El servidor esta funcionando en el puerto', app.get('port'));
 }
 );
+//conectar a BD
+mongoose.connect("mongodb+srv://node:gLafZY6rWHRHmzoE@clustersergio.ponuf.mongodb.net/tienda?retryWrites=true&w=majority")
+.then(bd=>console.log('BD conectada!'))
+.catch(err=>console.log(err));
+
+//establecer sistema de vistas
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+ 
+
+
+
 //desplegar mensaje de conectado
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const model = require('../models/ventas.js')();
+const Venta = require('../model/ventas.js');
+
+router.get('/', async (req, res) =>{
+    const datos = await Venta.find();
+    console.log(datos);
+    res.render('index.ejs',{
+        datos
+    });
+});
 
 module.exports = router;
