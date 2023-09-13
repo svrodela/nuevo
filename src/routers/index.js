558710b324c504c5a40ca41ec5e4a43c99f9b9de
svrodela <svrodela@gmail.com>
2023-09-13 10:28:21 -0600

    modificacion de campos bd, despliegue de tabla

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const model = require('../models/ventas.js')();
-const Venta = require('../model/ventas.js');
+const model = require('../models/datos')();
+const Venta = require('../models/datos');
 
 router.get('/', async (req, res) =>{
     const datos = await Venta.find();
-    console.log(datos);
+    console.log("los datos son".datos);
     res.render('index.ejs',{
         datos
     });
